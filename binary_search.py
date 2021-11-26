2228e17b801dcb415990a2d75d841c334d8b2ded
Woodstock <victorhe97@gmail.com>
2021-11-26 13:52:37 -0500

    Completed binary_search

---
+++ binary_search.py
@@ -0,0 +1,17 @@
+def binary_search(list, target):
+  first = 0
+  last = len(list - 1)
+
+  while first <= last:
+    midpoint = (first + last) // 2 # // is floor division in python
+
+    if list[midpoint] == target:
+      return midpoint
+    elif list[midpoint] < target:
+      first = midpoint + 1
+    else:
+      last = midpoint - 1
+
+
+
+
