c204a4744579356adc48838cac93eb7435d44cce
Woodstock <victorhe97@gmail.com>
2021-11-26 15:03:22 -0500

    Added the add and repr methods for linked list

--- python_dsa/linked_list.py
+++ python_dsa/linked_list.py
@@ -39,3 +39,32 @@ class LinkedList:
       current = current.next_node
 
     return count
+
+  def add(self, data):
+    """
+    Adds new Node containing data at head of the list
+    Takes O(1) time
+    """
+
+    new_node = Node(data)
+    new_node.next_node = self.head
+    self.head = new_node
+
+  def __repr__(self):
+    """
+    Return a string representation of the list
+    Takes O(n) time
+    """
+
+    nodes = []
+    current = self.head
+
+    while current:
+      if current is self.head:
+        nodes.append("[Head: %s]" % current.data)
+      elif current.next_node is None:
+        nodes.append("[Tail : %s]" % current.data)
+      else:
+        nodes.append("[%s]" % current.data)
+
+      current = current.next_node
